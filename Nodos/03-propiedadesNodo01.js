4a5de57e1de6b9324311721f5536cd749f845647
JoseRubicco21 <a23joserf@iessanclemente.net>
2024-11-12 10:12:08 +0100

    Added some nodes excercises

---
+++ Nodos/03-propiedadesNodo01.js
@@ -0,0 +1,49 @@
+'use-strict';
+/*
+Descarga o código fonte 03-propiedadesNodo01.html e indica, polo menos, unha
+forma de acceder ao seguinte contido e mostralo por consola:
+● O innerHTML, innerText e textContent da etiqueta “Escolle sexo”:
+● O valor do primeiro input de sexo
+● O valor do sexo que estea seleccionado.
+● O texto de cada un dos elementos <li>
+● Indica cantos elementos <li> hai.
+● Indica o valor do atributo data-widget-name
+*/
+
+// Label Escolle sexo
+const form = document.querySelector('form');
+const escolleSexoElement = form.lastElementChild.previousElementSibling;
+
+console.log(escolleSexoElement.innerHTML);
+console.log(escolleSexoElement.innerText);
+console.log(escolleSexoElement.textContent);
+
+// First input
+
+const firstInputOfEscolleSexo =
+	escolleSexoElement.firstElementChild.nextElementSibling;
+console.log(firstInputOfEscolleSexo.getAttribute('value')); // In the docs it's says getAttribute is a memory performant way of accessing attributes.
+
+// Checked input
+const [checkedInputOfEscolleSexo] = Array.from(escolleSexoElement.children)
+	.filter((element) => element.nodeName === 'INPUT')
+	.filter((element) => element.hasAttribute('checked'));
+console.log(checkedInputOfEscolleSexo);
+
+// Get text of each LI item in first UL.
+const listElement = document.querySelector('ul');
+const listItemsElements = Array.from(listElement.children)
+	.filter((element) => element.nodeName === 'LI')
+	.forEach((element) => console.log(element.innerText));
+
+// Get number of LIs in first UL
+
+const numberOfLisInUl = listElement.childElementCount;
+console.log(numberOfLisInUl);
+
+//
+
+const valueOfdataWidgetName = document
+	.querySelector('[data-widget-name]')
+	.getAttribute('data-widget-name');
+console.log(valueOfdataWidgetName);
---
+++ Nodos/03-propiedadesNodo02.js
@@ -0,0 +1,41 @@
+/*
+2. Descarga o código fonte 03-propiedadesNodo02.html e mostra por consola:
+● O número de ligazóns da páxina.
+● A dirección da penúltima ligazón.
+● O número de ligazóns que apuntan a http://proba
+● O número de ligazóns do terceiro parágrafo.
+● Modifica o estilo das ligazóns que apuntan a http://proba para que teñan o
+texto de cor laranxa.*/
+
+// 1
+const numbersOfLinks = document.getElementsByTagName('a').length;
+console.log(numbersOfLinks);
+
+// 2
+const beforeLastLink = Array.from(document.getElementsByTagName('a'))[
+	numbersOfLinks - 2
+].getAttribute('href'); // Retrieves N num of links starting from 1. Arrays are indexed at 0, so -1, to get the before last, -1. So -2.
+
+console.log(beforeLastLink);
+
+// 3
+const linksPointingToAdress = Array.from(
+	document.getElementsByTagName('a')
+).filter((element) => element.getAttribute('href') === 'http://proba').length;
+console.log(linksPointingToAdress);
+
+// 4
+
+const numberOfLinksInThirdParagraph = [
+	...[...document.getElementsByTagName('p')[2].getElementsByTagName('a')],
+].length;
+
+console.log(numberOfLinksInThirdParagraph);
+
+// 5
+
+const linkElementsPointingToAdress = [...document.getElementsByTagName('a')]
+	.filter((element) => element.getAttribute('href') === 'http://proba')
+	.forEach((element) => element.setAttribute('style', 'color: orange;'));
+
+console.log(linkElementsPointingToAdress);
