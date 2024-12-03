69eb23879c825a814b97ca0d194a3d338b3fbd1a
JoseRubicco21 <a23joserf@iessanclemente.net>
2024-12-03 09:23:02 +0100

    Finished gameState return. It's technically playable

--- tres_en_raya/main.js
+++ tres_en_raya/main.js
@@ -2,30 +2,31 @@
 
 /* Player 1 : 1, Machine : -1, Empty : 0 */
 const board = [
-	[0, 0, 1],
-	[0, 1, 1],
-	[1, 0, 1],
+	[-1, 0, 1],
+	[0, -1, 0],
+	[1, 0, -1],
 ];
 
 // negative needs = [i][j+2] == [i+1][j+1] === [i+2][j]
 const validateRow = (board) => {
 	for (row of board) {
-		if (row[0] === row[1] && row[1] === row[2]) return true;
+		if (row[0] != 0 && row[0] === row[1] && row[1] === row[2])
+			return { state: true, player: row[0] };
 	}
-
-	return false;
+	return { state: false, player: 0 };
 };
 
 const validateCols = (board) => {
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < board[i].length; j++) {
 			if (
+				board[i][j] != 0 &&
 				board[i][j] === board?.[i + 1][j] &&
 				board[i][j] === board?.[i + 2][j]
 			)
-				return true;
+				return { state: true, player: board[i][j] };
 		}
-		return false;
+		return { state: false, player: 0 };
 	}
 };
 
@@ -33,53 +34,58 @@ const validatePositiveDiagonal = (board) => {
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < board[i].length; j++) {
 			if (
+				board[i][j] != 0 &&
 				board[i][j] === board?.[i + 1]?.[j + 1] &&
 				board[i][j] === board?.[i + 2]?.[j + 2]
 			)
-				return true;
+				return { state: true, player: board[i][j] };
 		}
 	}
-	return false;
+	return { state: false, player: 0 };
 };
 
 const validateNegativeDiagonal = (board) => {
 	for (let i = 0; i < board.length; i++) {
 		for (let j = 0; j < board[i].length; j++) {
 			if (
+				board[board.length - 1][board.length - 1] != 0 &&
 				board[board.length - 1][board.length - 1] ===
 					board?.[board.length - 1]?.[board.length - 1] &&
 				board[board.length - 1][board.length - 1] ===
 					board?.[board.length - 2]?.[board.length - 2]
 			)
-				return true;
+				return {
+					state: true,
+					player: board[board.length - 1][board.length - 1],
+				};
 		}
 	}
-	return false;
+	return { state: false, player: 0 };
 };
 
 const validateBoard = (board, [...validations]) => {
-	let gameState = false;
-
-	for (let i = 0; i < validations.length; i++) {
-		if (gameState == true) return gameState;
-		gameState = validations[i](board);
-		console.log(gameState);
+	for (const validationStrat of validations) {
+		gameState = validationStrat(board);
+		if (gameState.state == true) {
+			switch (validationStrat.name) {
+				case 'validateRow':
+					console.log('Won at a row');
+					return gameState;
+				case 'validateCols':
+					console.log('Won at a col');
+					return gameState;
+				case 'validatePositiveDiagonal':
+					console.log('Won at pos diag');
+					return gameState;
+				case 'validateNegativeDiagonal':
+					console.log('Won at neg diag');
+					return gameState;
+				default:
+					return gameState;
+			}
+		}
 	}
-
-	return gameState;
 };
-
-validateBoard(board, [
-	validateRow,
-	validateCols,
-	validatePositiveDiagonal,
-	validateNegativeDiagonal,
-]);
-
-/* 0,1,2,
-   3,4,5,
-   6,7,8
-*/
 
 /** Random AI difficulty */
 
@@ -101,4 +107,11 @@ const validatePossibleMove = (board) => {
     Make one that is unwinnable, as it's always tieable? 
 */
 
-console.log(validatePossibleMove(board));
+console.log(
+	validateBoard(board, [
+		validateRow,
+		validateCols,
+		validatePositiveDiagonal,
+		validateNegativeDiagonal,
+	])
+);
